cfb691a92d8d6439b7b4dd899b2e325b2ac4014a
Darren SooHoo <dsoohoo@MossBeachHomes.com>
2019-01-08 11:41:42 -0800

    Checkpoint 2761 Player Bar Range Inputs complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -31,14 +31,14 @@ class Album extends Component {
               this.setState({ currentTime: this.audioElement.currentTime });
             },
             durationchange: e => {
-                this.setState({ duration: this.audioElement.duration });
+              this.setState({ duration: this.audioElement.duration });
             }
-        };
-        this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
-        this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
-    }
+          };
+          this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+          this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+      }
         
-    componentWillUnmount() {
+      componentWillUnmount() {
         this.audioElement.src = null;
         this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
         this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
