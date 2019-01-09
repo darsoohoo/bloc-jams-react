55e003c13df438df2a01d49d33aeba528ac41db4
Darren SooHoo <dsoohoo@MossBeachHomes.com>
2019-01-09 10:02:59 -0800

    Assignment 2761 PlayerBarRangeInputs Complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -27,25 +27,6 @@ class Album extends Component {
     }
 
 
-/*
-    formatTime(seconds) {
-        if (isNaN(seconds)) { return "-:--"; }
-        const wholeSeconds = Math.floor(seconds);
-        const minutes = Math.floor(wholeSeconds / 60);
-        const remainingSeconds = wholeSeconds % 60;
-        let output = minutes + ':';
-    
-        if (remainingSeconds < 10) {
-          output += '0';
-        }
-        output += remainingSeconds;
-        return output;
-    }*/
-    
-
-
-
-
     componentDidMount() {
         this.eventListeners = {
             timeupdate: e => {
